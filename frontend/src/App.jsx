be36844b8bf84487dc87d01d5696a2a08905295c
skald1311 <hoangmanhduong01@gmail.com>
2024-05-16 23:06:27 +0700

    install styled-components + add global styles

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,55 @@
-import { useEffect } from "react";
+// import { useEffect } from "react";
+// useEffect(function () {
+//   fetch(`http://127.0.0.1:8000/user/login/`)
+//     .then((res) => res.json())
+//     .then((data) => console.log(data));
+// }, []);
+
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 500;
+  border: none;
+  border-radius: 7px;
+  background-color: purple;
+  color: white;
+  cursor: pointer;
+  margin: 20px;
+`;
+
+const Input = styled.input`
+  border: 1px solid #ddd;
+  border-radius: 5px;
+  padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
 
 function App() {
-  useEffect(function () {
-    fetch(`http://127.0.0.1:8000/user/login/`)
-      .then((res) => res.json())
-      .then((data) => console.log(data));
-  }, []);
-  return <div>App</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>Cooksta</H1>
+        <Input placeholder="username"></Input>
+        <Input placeholder="password"></Input>
+        <Button>Login</Button>
+        <Button>Sign up</Button>
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
